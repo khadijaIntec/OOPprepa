6af4946265cb0baf74a3212153942fa99a88705a
Sadki Khadija <Sadki.Khadija@student.intecbrussel.be>
2021-01-13 18:08:26 +0100

    Add project files.

---
+++ AbstractClasse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasse
+{
+    public abstract class parent
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string SayHello()
+        {
+            return "Hello World";
+        }
+        public abstract decimal GetSalary();
+    }
+    public class child : parent
+    {
+        public override decimal GetSalary()
+        {
+            return 3250.50m;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            child c = new child()
+            {
+                Age = 25,
+                FirstName = "Khadija",
+                LastName = "sadki"
+            };
+            Console.WriteLine($"First Name {c.FirstName} Last Name{c.LastName}");
+            decimal salary = c.GetSalary();
+            Console.WriteLine($"salary : {salary}");
+        }
+    }
+}
---
+++ OOPprepa/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPprepa
+{
+    public class Persoon
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+        public Persoon()
+        {
+            Age = 15;
+        }
+        public Persoon(string fullName) : this()
+        {
+            this.FullName = fullName;
+        }
+        public Persoon(string fullName, int age) : this(fullName)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+
+        public void GetDetails()
+        {
+            Console.WriteLine($"The Fullname is :{FullName} and the Age is {Age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoon p = new Persoon
+            {
+
+                FullName ="Khadija Sadki",
+                Age = 28
+            };
+                 p.GetDetails();
+            Console.ReadKey();
+          
+        }
+    }
+}
